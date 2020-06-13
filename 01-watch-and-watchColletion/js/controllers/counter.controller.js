407215f7f2bb413036c0bdcd0711ae31c4f18e24
Daniel Canqui <dcanqui@hinda.com>
2020-06-13 19:21:47 -0400

    Added function to increment and decrement and watch the count variable when changes.

--- 01-watch-and-watchColletion/js/controllers/counter.controller.js
+++ 01-watch-and-watchColletion/js/controllers/counter.controller.js
@@ -1,5 +1,17 @@
-function CounterController() {
+function CounterController($scope) {
+  this.count = 0;
+  this.increment = function () {
+    this.count++;
+  }
+  this.decrement = function () {
+    this.count--;
+  }
 
+  $scope.$watch(angular.bind(this, function () {
+    return this.count;
+  }), function(newValue, oldValue) {
+    console.log(newValue, oldValue);
+  });
 }
 
 angular
