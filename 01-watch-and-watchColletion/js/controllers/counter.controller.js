f2bb324651f72be302f7fa48f41624535603e5bc
Daniel Canqui <dcanqui@hinda.com>
2020-06-14 15:10:49 -0400

    added watch Collection.

--- 01-watch-and-watchColletion/js/controllers/counter.controller.js
+++ 01-watch-and-watchColletion/js/controllers/counter.controller.js
@@ -1,16 +1,27 @@
 function CounterController($scope) {
   this.count = 0;
+  this.listCount = []
   this.increment = function () {
     this.count++;
+    this.listCount.unshift({id: this.count});
   }
   this.decrement = function () {
     this.count--;
+    this.listCount.unshift({id: this.count});
   }
 
   $scope.$watch(angular.bind(this, function () {
     return this.count;
   }), function(newValue, oldValue) {
-    console.log(newValue, oldValue);
+     if (newValue !== oldValue)
+      console.log(newValue, oldValue);
+  });
+
+  $scope.$watchCollection(angular.bind(this, function () {
+    return this.listCount;
+  }), function(newValue, oldValue) {
+    if (newValue !== oldValue)
+      console.log(newValue, oldValue);
   });
 }
 
