7d635b164ecbed129736ac3b7bc5c0772a6058e1
Daniel Canqui <dcanqui@hinda.com>
2020-06-14 16:50:49 -0400

    Added observe and update attr

--- 02-attrs-observe/js/directives/counter.directive.js
+++ 02-attrs-observe/js/directives/counter.directive.js
@@ -1,12 +1,26 @@
 function counter() {
   return {
     template: `
-      <a>increment</a>
-      -
-      <a>decrement</a>
+      <div class="counter">
+        <h3>Counter: {{counter.count}}</h3>
+        <div>
+          <a href="" ng-click="counter.increment()">increment</a>
+          -
+          <a href="" ng-click="counter.decrement()">decrement</a>
+          </br>
+          <a href="" ng-click="counter.updateNameFood()">Update Name</a>
+        </div>
+      </div>
     `,
     link: function(scope, element, attr) {
-
+      attr.$observe('name', function (value) {
+        if (value === 'Blue food') {
+          attr.$updateClass('blue-food', 'red-food');
+        }
+        if (value === 'red food') {
+          attr.$updateClass('red-food','blue-food');
+        }
+      });
     }
   };
 }
