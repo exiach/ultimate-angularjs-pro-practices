e9c8cc28df935393e66a265b015747de6f505af5
Daniel Canqui <dcanqui@hinda.com>
2020-07-09 21:36:42 -0400

    transclution with $transclude.

--- 06-transclude/js/directives/contac-card.directive.js
+++ 06-transclude/js/directives/contac-card.directive.js
@@ -3,10 +3,21 @@ function contacCard() {
     transclude: true,
     template: `
       <div>
-        <h1>Contact</h1>
-        <div ng-transclude></div>
+        <span>Contact</span>
+        <div></div>
       </div>
-    `
+    `,
+    link: function ($scope, $element, $attribute, $ctrl, $transclude) {
+      var div = $element.find('div');
+      var span = $element.find('span');
+      var cloned = $transclude(function (element) {
+        element[1].textContent = element[1].textContent.toUpperCase();
+      });
+      console.log(cloned);
+      span.append(cloned[1]);
+      div.append(cloned[3]);
+      
+    }
   };
 }
 
